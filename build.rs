c8ff206cabbb9b881d298b178519dfd728ee514e
m.zagoruyko <wlir0tn@gmail.com>
2025-05-05 17:38:46 +0300

    [~] in progress, fixed int8 capability bug

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    SpirvBuilder::new("levenshtein_shader", "spirv-unknown-spv1.4")
+    SpirvBuilder::new("levenshtein_shader", "spirv-unknown-vulkan1.2")
+        .capability(spirv_builder::Capability::Int8)
+        // .extension("SPV_KHR_8bit_storage")
         .print_metadata(MetadataPrintout::Full)
         .build()?;
     Ok(())
--- levenshtein_shader/src/lib.rs
+++ levenshtein_shader/src/lib.rs
@@ -1,9 +1,7 @@
 #![no_std]
 
 use core::cmp::min;
-
-use glam::UVec3;
-use spirv_std::{glam, spirv};
+use spirv_std::spirv;
 
 //fill the words up? the padding rn is 64
 const MAX: usize = 64;
@@ -11,49 +9,47 @@ const MAX: usize = 64;
 /// Возвращает метрику, разность по модулю между двумя последовательностями символов
 pub fn levenshtein(words: &[u32]) -> Option<usize> {
     // String - UTF-8–encoded
-    // problems: multi-byte characters in utf-8 
+    // problems: multi-byte characters in utf-8
     // this only works for one byte per one character
     // to note: doesn't support slicing
+    if words.len() < MAX * 2 {
+        return None;
+    }
 
     let n = MAX;
 
-    let mut prev: [usize; MAX + 1] = [0usize; MAX + 1];
-    let mut curr: [usize; MAX + 1] = [0usize; MAX + 1];
+    let mut prev: [usize; MAX + 1] = [0; MAX + 1];
+    let mut curr: [usize; MAX + 1] = [0; MAX + 1];
 
-    for j in 0usize..=n { 
-        prev[j] = j as usize; 
+    for j in 0..=MAX { 
+        prev[j] = j; 
     }
 
-    let mut i: usize = 0;
-    let mut j: usize = 0;
-
     // базовый алгоритм левенштейна из википедии
-    for a_byte in 0usize..=MAX {
-        curr[0] = (i + 1usize) as usize;
-        i += 1usize;
-        for j in 0usize..=MAX {
-            let cost  = (words[a_byte] != words[j]) as usize;
-            // curr[j + 1usize] = min(
-            //     // error: `i8` without `OpCapability Int8`
-            //     min(curr[j] + 1usize,
-            //         prev[j + 1usize] + 1usize),
-            //     prev[j] + cost
-            // );
+    for i in 0..MAX {
+        curr[0] = (i + 1) as usize;
+        let a = words[i]; 
+        for j in 0..MAX {
+            //offset
+            let b =  words[MAX + j];
+            let cost = if a != b { 1 } else { 0 };
+            curr[j + 1] = min(min(curr[j] + 1, prev[j + 1] + 1), prev[j] + cost);
         }
-        for j in 0usize..=n { prev[j] = curr[j]; }
+        for j in 0..=n {
+            prev[j] = curr[j];
+        }
     }
     Some(prev[n])
 }
 
 #[spirv(compute(threads(1)))]
 pub fn main_cs(
-    //leave that out for now
-    #[spirv(global_invocation_id)] _id: UVec3,
-    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] words: & mut [u32], // слова единым array
-    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] output: & mut usize, // метрика левенштейна для пар слов
+    // #[spirv(global_invocation_id)] id: UVec3,
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] words: &mut [u32], // слова единым array
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] output: &mut usize, // метрика левенштейна для пар слов
 ) {
     //current invocation index
     // let index = id.x as usize;
 
-    *output = levenshtein(words).unwrap_or_default();
+    *output = levenshtein(words).unwrap_or(100);
 }
